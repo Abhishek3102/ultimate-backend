712da0bad5e7e0269ab7ff936743129ec9619770
Abhishek3102 <ankushpan694@gmail.com>
2025-05-22 21:46:36 +0530

    feat : like comment and healthcheck all todos done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,20 +7,149 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const {page = 1, limit = 10} = req.query;
 
-})
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video ID");
+    }
+
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+
+    const comments = await Comment.find({comment_video : videoId})
+    .populate("user", "fullName avatar")
+    .sort({ craetedAt : -1})
+    .skip(skip)
+    .limit(parseInt(limit));
+
+    const totalComments = await Comment.countDocuments({ comment_video: videoId});
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            total,
+            page : parseInt(page),
+            limit : parseInt(limit),
+            comments,
+        }, "Comments fetched successfully")
+    );
+});
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Video ID");
+    }
+
+    const userId = req.user._id;
+    const { content } = req.body;
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid User ID");
+    }
+
+    if(!content || typeof content != "string" || content.trim() === ""){
+        throw new ApiError(400, "Comment content is required and must be a non-empty string");
+    }
+
+    try {
+        await Comment.create({
+            comment_content : content,
+            user : userId, 
+            comment_video : videoId,
+        });
+    
+        return res.status(201).json(
+            new ApiResponse(201, null, "Comment created successfully")
+        )
+    
+    } catch (error) {
+        throw new ApiError(500, "Error while creating comment");
+    }
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
-})
+    const userId = req.user._id;
+    const { content } = req.body;
+    const {videoId} = req.params;
+    const { commentId } = req.params;
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Video ID");
+    }
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid User ID");
+    }
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+
+    try {
+        const comment = await Comment.findById(commentId);
+    
+      if (!comment) {
+        throw new ApiError(404, "Comment not found");
+      }
+    
+        if(!content || typeof content != "string" || content.trim() === ""){
+            throw new ApiError(400, "Comment content is required and must be a non-empty string");
+        }
+    
+        if (comment.user.toString() !== userId.toString()) {
+            throw new ApiError(404, "You are not authorized to update this comment");
+        }
+    
+        comment.comment_content = content;
+        await comment.save();
+    
+        return res.status(200).json(
+            new ApiResponse(200, null, "Comment updated successfully")
+        );
+    } catch (error) {
+        throw new ApiError(500, "Error while updating comment");
+    }
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const userId = req.user._id;
+    const { content } = req.body;
+    const {videoId} = req.params;
+    const { commentId } = req.params;
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
+
+    try {
+        const comment = await Comment.findById(commentId);
+    
+      if (!comment) {
+        throw new ApiError(404, "Comment not found");
+      }
+    
+      if (comment.user.toString() !== userId.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this comment");
+      }
+    
+      await comment.deleteOne();
+    
+      return res.status(200).json(
+        new ApiResponse(200, null, "Comment deleted successfully")
+      );
+    } catch (error) {
+        throw new ApiError(500, "Error deleting comment");
+    }
+
 })
 
 export {
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -24,7 +24,6 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
             new ApiResponse(200, null, "Like removed successfully")
         );
     }
-    else{
         await Like.create({
             liked : videoId,
             user : userId,
@@ -33,7 +32,7 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         return res.status(201).json(
             new ApiResponse(201, null, "Like added to the Video")
         );
-    }
+    
 
 
 })
@@ -41,17 +40,101 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if (!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid Comment ID");
+    }
 
-})
+    const userId = req.user._id;
+
+    try {
+        const alreadyLikedComment = await Like.findOne({
+            likedComment : commentId,
+            user : userId,
+        });
+    
+        if(alreadyLikedComment){
+            await alreadyLikedComment.deleteOne();
+            return res.status(200).json(
+                new ApiResponse(200, null, "Like removed from comment")
+            );
+        }
+    
+        else{
+            await Like.create({
+                likedComment : commentId,
+                user : userId,
+            });
+    
+            return res.status(201).json(
+                new ApiResponse(201, null, "Like added to the Comment")
+            );
+        }
+    
+    } catch (error) {
+        throw new ApiError(500, "Failed to toggle like on comment");
+    }
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+
+    const userId = req.user._id;
+
+    try {
+        const alreadyLikedTweet = await Like.findOneAndDelete({
+            likedTweet : tweetId,
+            user : userId,
+        });
+
+        if(alreadyLikedTweet){
+            return res.status(200).json(
+                new ApiResponse(200, null, "Liked removed from tweet")
+            );
+        }
+            await Like.create({
+                likedTweet : tweetId,
+                user : userId,
+            });
+
+            return res.status(201).json(
+                new ApiResponse(201, null, "Like added to tweet")
+            )
+        
+    } catch (error) {
+        throw new ApiError(500, "Error while toggling like on Tweet");
+    }
 }
-)
+);
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const userId = req.user._id;
+
+    if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user ID");
+  }
+
+    try {
+        const allLikedVideos = await Like.find({user : userId})
+        .populate("video", "title description url thumbnail")
+        .select("video createdAt");
+    
+        if (!allLikedVideos.length){
+            return res.status(404).json(
+                new ApiResponse(404, "No liked videos found")
+            );
+        }
+    
+        return res.status(200).json(
+            new ApiResponse(200, allLikedVideos, "Liked videos fetched successfully")
+        );
+    } catch (error) {
+       throw new ApiError(500, null, "Error fetching liked videos");
+    }
 })
 
 export {
